5f00533da0fe2d2285f9902bb09c9e43e12ac8ca
joaoparaujocr <araujo.joao.paulo.jaa@gmail.com>
2025-04-19 15:15:24 -0400

    test: add test for user information route

---
+++ backend/spec/requests/users/me_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "/users/me", type: :request do
+  let!(:user_correct) { create(:user_correct) }
+
+  it "should return information about the logged in user" do
+    post '/login', params: { user: { email: user_correct.email, password: user_correct.password } }
+
+    token = JSON.parse(response.body)['status']['token']
+
+    get '/users/me', headers: { 'Authorization': "Bearer #{token}" }
+
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body)).to include_json(
+      status: {
+        code: 200,
+        message: 'Successful!'
+      },
+      data: {
+        id: be_a(Integer),
+        email: be_a(String),
+        name: be_a(String)
+      }
+    )
+  end
+end
